30dfe49a0b53307dbfdfe69a97f53fac85b13032
mohanpyc <mohanpyc@gmail.com>
2024-05-21 03:44:12 -0500

    added post method for signup and login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const cors = require('cors');
+const User = require('./models/user');
 
 const app = express();
 
@@ -12,9 +13,65 @@ app.use(bodyParser.json());
 
 app.use(express.json({ extended: false }));
 
-// app.use(cors({
-//   origin: 'http://localhost:3000'  // Allow requests from this origin
-// }));
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        let user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ msg: 'Invalid Credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Invalid Credentials' });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+app.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+    console.error(username,password);
+    try {
+        let user = await User.findOne({ username });
+        if (user) {
+            console.error('userAlready exists');
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+
+        user = new User({
+            username,
+            password
+        });
+
+        await user.save();
+        console.error('signup successful');
+        res.send('Signup successful');
+    } catch (err) {
+        console.error(err.message,'in catch');
+        res.status(500).send('Server error');
+    }
+});
+
 
 app.use(cors());
 
--- models/user.js
+++ models/user.js
@@ -1,15 +1,47 @@
+// const mongoose = require('mongoose');
+
+// const UserSchema = new mongoose.Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   }
+// });
+
+// module.exports = mongoose.model('User', UserSchema);
+
+
+// models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+// Hash the password before saving the user
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
+
